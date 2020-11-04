3260da55cb3e26a9c75a480f445c1f5e3bb112d7
pratisthasthapit <psthapit@deakin.edu.au>
2020-11-04 19:03:59 +1100

    Add dashboard card

--- covid-dashboard/src/App.js
+++ covid-dashboard/src/App.js
@@ -1,9 +1,62 @@
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import CardDeck from "react-bootstrap/CardDeck";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      COVID-19 Dashboard
+  return(
+  <div>
+      <br/>
+      <h2 style={{textAlign: "center"}}>COVID-19 Dashboard</h2>
+      <br/>
+
+      <CardDeck>
+        <Card 
+          className="text-center" 
+          bg="light" 
+          text={"dark"} 
+          style={{margin: "10px"}}
+        >
+          <Card.Body>
+            <Card.Title>Cases</Card.Title>
+            <Card.Text>
+              xyz
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated </small>
+          </Card.Footer>
+        </Card>
+        <Card 
+          className="text-center" 
+          bg="light" 
+          text={"danger"} 
+          style={{margin: "10px"}}
+        >
+          <Card.Body>
+            <Card.Title>Deaths</Card.Title>
+            <Card.Text>
+            xyz
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card 
+          className="text-center" 
+          bg="light" 
+          text={"success"} 
+          style={{margin: "10px"}}
+        >
+          <Card.Body>
+            <Card.Title>Recovered</Card.Title>
+          </Card.Body>
+          <Card.Footer>
+            <small>xyz</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
     </div>
   );
 }
